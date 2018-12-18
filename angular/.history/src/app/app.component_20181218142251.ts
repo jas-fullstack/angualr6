64a753bd4c26a32f199b11774e0605ab06ca03db
Sanjeev Kumar <sanjeevkumar@smartdatainc.net>
2018-12-18 17:30:39 +0530

    databinding

---
+++ angular/.history/src/app/app.component_20181218142251.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  fruit : string = 'apple';
+  one : number = 100;
+  two : number = 50
+  addnumber(){
+      return this.one + this.two;
+  }
+}
---
+++ angular/.history/src/app/app.component_20181218171558.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  fruit : string = 'apple';
+  one : number = 100;
+  two : number = 50
+  imgSrc : string = "https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+  addnumber(){
+      return this.one + this.two;
+  }
+  function onSave(params:type) {
+    
+  }
+}
